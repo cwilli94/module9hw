04edb53065be75a6f7775a486ca61ca444d8fc07
conner <conner.williams4@gmail.com>
2023-05-23 12:14:14 -0400

    completed app

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,76 @@
-// TODO: Include packages needed for this application
+// // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Provide a brief description of your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use:",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Provide guidelines for contributing to your project:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Provide instructions for running tests:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3-Clause", "None"],
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("README.md file generated successfully!");
+    }
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdownContent = generateMarkdown(answers);
+    writeToFile("README.md", markdownContent);
+  });
+}
 
-// Function call to initialize app
+// // Function call to initialize app
 init();
